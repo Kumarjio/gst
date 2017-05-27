1063015afd3975c5bbdf56acac823f0cd4ae4ccd
Nabi Arshad <nabiarshad@gmail.com>
2017-05-27 19:39:40 +0100

    Cleanup code

--- application/views/templates/includes/header.php
+++ application/views/templates/includes/header.php
@@ -5,61 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<base href="<?php echo base_url();?>">
 <?php $this->load->view('templates/includes/meta'); ?>
-
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
-	
-	<!-- Animate.css -->
 	<link rel="stylesheet" href="assets/frontend/css/animate.css">
-	<!-- Icomoon Icon Fonts-->
 	<link rel="stylesheet" href="assets/frontend/css/icomoon.css">
 	<link rel="stylesheet" href="assets/frontend/css/font-awesome.min.css">
-	<!-- Bootstrap  -->
 	<link rel="stylesheet" href="assets/frontend/css/bootstrap.css">
-	<!-- Superfish -->
 	<link rel="stylesheet" href="assets/frontend/css/superfish.css">
-	<!-- Magnific Popup -->
 	<link rel="stylesheet" href="assets/frontend/css/magnific-popup.css">
-	<!-- Date Picker -->
 	<link rel="stylesheet" href="assets/frontend/css/bootstrap-datepicker.min.css">
-	<!-- CS Select -->
 	<link rel="stylesheet" href="assets/frontend/css/cs-select.css">
 	<link rel="stylesheet" href="assets/frontend/css/cs-skin-border.css">
-	
 	<link rel="stylesheet" href="assets/frontend/css/style.css">
 	<link rel="stylesheet" href="assets/frontend/css/custom.css">
-    <link href="assets/frontend/css/button_color.css" rel="stylesheet">
-
-
-	<!-- Modernizr JS -->
+  <link href="assets/frontend/css/button_color.css" rel="stylesheet">
 	<script src="assets/frontend/js/modernizr-2.6.2.min.js"></script>
 	<!-- FOR IE9 below -->
 	<!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
 	<![endif]-->
-
-
 	<script src="assets/frontend/js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
 	<script src="assets/frontend/js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
 	<script src="assets/frontend/js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
 	<script src="assets/frontend/js/jquery.waypoints.min.js"></script>
 	<script src="assets/frontend/js/sticky.js"></script>
-
-	<!-- Stellar -->
 	<script src="assets/frontend/js/jquery.stellar.min.js"></script>
-	<!-- Superfish -->
 	<script src="assets/frontend/js/hoverIntent.js"></script>
 	<script src="assets/frontend/js/superfish.js"></script>
-	<!-- Magnific Popup -->
 	<script src="assets/frontend/js/jquery.magnific-popup.min.js"></script>
 	<script src="assets/frontend/js/magnific-popup-options.js"></script>
-	<!-- Date Picker -->
 	<script src="assets/frontend/js/bootstrap-datepicker.min.js"></script>
-	<!-- CS Select -->
 	<script src="assets/frontend/js/classie.js"></script>
 	<script src="assets/frontend/js/selectFx.js"></script>
-	
-	<!-- Main JS -->
+	<script src="gofly/js/gstmain.js"></script>
 	</head>
--- application/views/templates/index.php
+++ application/views/templates/index.php
@@ -1,131 +1,17 @@
-
 <?php $this->load->view('templates/includes/header');?>
 	<body>
 		<div id="fh5co-wrapper">
 		<div id="fh5co-page">
 
 <?php $this->load->view('templates/includes/menu');?>
-	
-<?php  $this->load->view('templates/includes/slider');?>
-		
+<?php $this->load->view('templates/includes/slider');?>
 <?php $this->load->view('templates/includes/home_service');?>
-		
-		<!--<div id="fh5co-features">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4 animate-box">
-
-						<div class="feature-left">
-							<span class="icon">
-								<i class="icon-hotairballoon"></i>
-							</span>
-							<div class="feature-copy">
-								<h3>Family Travel</h3>
-								<p>This is description. This is description. This is description. </p>
-								<p><a href="#">Learn More</a></p>
-							</div>
-						</div>
-
-					</div>
-
-					<div class="col-md-4 animate-box">
-						<div class="feature-left">
-							<span class="icon">
-								<i class="icon-search"></i>
-							</span>
-							<div class="feature-copy">
-								<h3>Travel Plans</h3>
-								<p>This is description. This is description. This is description. </p>
-								<p><a href="#">Learn More</a></p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 animate-box">
-						<div class="feature-left">
-							<span class="icon">
-								<i class="icon-wallet"></i>
-							</span>
-							<div class="feature-copy">
-								<h3>Honeymoon</h3>
-                                <p>This is description. This is description. This is description. </p>
-
-								<p><a href="#">Learn More</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-4 animate-box">
-
-						<div class="feature-left">
-							<span class="icon">
-								<i class="icon-wine"></i>
-							</span>
-							<div class="feature-copy">
-								<h3>Business Travel</h3>
-								<p>This is description. This is description. This is description. </p>
-								<p><a href="#">Learn More</a></p>
-							</div>
-						</div>
-
-					</div>
-
-					<div class="col-md-4 animate-box">
-						<div class="feature-left">
-							<span class="icon">
-								<i class="icon-genius"></i>
-							</span>
-							<div class="feature-copy">
-								<h3>Solo Travel</h3>
-								<p>This is description. This is description. This is description. </p>
-								<p><a href="#">Learn More</a></p>
-							</div>
-						</div>
-
-					</div>
-					<div class="col-md-4 animate-box">
-						<div class="feature-left">
-							<span class="icon">
-								<i class="icon-chat"></i>
-							</span>
-							<div class="feature-copy">
-								<h3>Explorer</h3>
-								<p>This is description. This is description. This is description. </p>
-								<p><a href="#">Learn More</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>-->
-
 <?php $this->load->view('templates/includes/home_place');?>
-
 <?php $this->load->view('templates/includes/home_client');?>
-
-		
-		<!-- fh5co-blog-section -->
-		
-<?php
-//$this->load->view('templates/includes/home_trip');
-?>
-
-		
 <?php
 $this->load->view('templates/includes/footer');
 ?>
-
-	
-
 	</div>
-	<!-- END fh5co-page -->
-
 	</div>
-	<!-- END fh5co-wrapper -->
-
-	<!-- jQuery -->
-
-
 	</body>
 </html>
-
