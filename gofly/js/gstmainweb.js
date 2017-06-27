a7154f252d235003d448a7fb65ec8e70ff2f7f4a
Nabi Arshad <nabiarshad@gmail.com>
2017-06-27 14:04:35 +0100

    Updating ad script for affiliate and website

--- gofly/js/gstmainweb.js
+++ gofly/js/gstmainweb.js
@@ -1,7 +1,41 @@
 $(document).ready(function () {
 	//var isDesktop = !(/Android|webOS|iPhone|iPad|BlackBerry|Windows Phone|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent));
+	//var getURL = window.location.href;
+	var getURLPath = window.location.pathname;
 	var getClientWidth = document.body.clientWidth;
 	var interval = 60000;
+	
+	//switch to check what page is being viewed:
+	switch (getURLPath) {
+	 case "/flights":
+        setPath = "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=7";
+        break; 
+    case "/hotels":
+    		setPath = "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=8";
+    		break;
+    case "/carhire":
+    		setPath = "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=9";
+    		break;
+    case "/holidays":
+    		setPath = "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=10";
+    		break;
+    case "/hajj":
+    		setPath = "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=11";
+    		break;
+		case "/umrah":
+    		setPath = "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=12";
+				break;
+		case "/travel-insurance":
+    		setPath = "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=13";
+    		break;
+    case "/cruises":
+    		setPath = "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=14";
+    		break;
+
+    default: 
+        setPath = "/";
+	}
+
 	//top banner refresh function
 	var TopRefresh = function () {
 		if (getClientWidth >= 468) {
@@ -13,7 +47,6 @@ $(document).ready(function () {
 					setTimeout(function () {
 					  TopRefresh();
 					}, interval);
-					//console.log(getClientWidth);
 				}
 			});
 		}
@@ -29,7 +62,6 @@ $(document).ready(function () {
 						setTimeout(function () {
 							TopRefresh();
 						}, interval);
-						//console.log("less than 370");
 					}
 				});
 			}
@@ -44,7 +76,6 @@ $(document).ready(function () {
 						setTimeout(function () {
 							TopRefresh();
 						}, interval);
-						//console.log("more than 370");
 					}
 				});
 			}
@@ -52,48 +83,43 @@ $(document).ready(function () {
 	};
 	TopRefresh();
 
-
-
 //left banner refresh function
 	var LeftRefresh = function () {
 		//only show if its desktop
 		if (getClientWidth >= 468) {
 			$.ajax({
-				url: "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=4",
+				url: setPath,
 				cache: false,
 				success: function (html) {
 					$('#leftBanner').html(html);
 					setTimeout(function () {
 						LeftRefresh();
 					}, interval);
-					//console.log("leftRefresh called ");
 				}
 			});
 		}
 	};
 	LeftRefresh();
 
-
 	//right banner refresh function
 	var RightRefresh = function () {
 		//only show if its desktop
 		if (getClientWidth >= 468) {
 			$.ajax({
-				url: "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=2",
+				//url: "//ads.gosearchtravel.com/www/delivery/afr.php?zoneid=2",
+				url: setPath,
 				cache: false,
 				success: function (html) {
 					$('#rightBanner').html(html);
 					setTimeout(function () {
 						RightRefresh();
 					}, interval);
-					//console.log("RightRefresh called ");
 				}
 
 			});
 		}
 	};
 	RightRefresh();
-
 
 $(window).scroll(function() {
 	 if ($(this).scrollTop() > 280){
